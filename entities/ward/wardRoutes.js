ad94cd8c93ec40766920d8622c37a9add77efa89
Code-Gale <codewhiz001@gmail.com>
2024-05-28 13:43:42 +0100

    improved route naming

--- entities/ward/wardRoutes.js
+++ entities/ward/wardRoutes.js
@@ -1,7 +1,7 @@
 const router = require('express').Router()
 const wardController = require('./wardController')
 
-router.post('/wards/new', wardController.createWard)
+router.post('/wards/', wardController.createWard)
 router.get('/wards', wardController.getAllWards)
 router.get('/wards/:id', wardController.getWardById)
 router.get('/departments/:id/wards', wardController.getWardsByDepartment)
